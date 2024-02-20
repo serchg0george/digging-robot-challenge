337804fc292f0bd0093650779763303bd34e6f68
serchg0george <serchg0george@gmail.com>
2024-02-20 20:12:14 +0200

    Implemented POST methods for excavateStones(), approximateDays()

--- src/main/java/com/comsystem/homework/rest/RobotRestController.java
+++ src/main/java/com/comsystem/homework/rest/RobotRestController.java
@@ -1,26 +1,29 @@
 package com.comsystem.homework.rest;
 
 import com.comsystem.homework.model.RobotPlan;
+import com.comsystem.homework.robot.RobotOperations;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.ErrorResponseException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import com.comsystem.homework.robot.RobotOperations;
 
 @RestController()
 @RequestMapping("/api/v1/robot/operation")
 public final class RobotRestController {
+
+    RobotOperations robotOperations;
 
     /**
      * This method exposes the functionality of {@link RobotOperations#excavateStonesForDays(int)} via HTTP
      */
     @PostMapping("/excavation")
     public ResponseEntity<RobotPlan> excavateStones(@RequestParam Integer numberOfDays) {
-        // TODO
-        throw new ErrorResponseException(HttpStatus.I_AM_A_TEAPOT);
+        RobotPlan robotPlan = new RobotPlan(numberOfDays,
+                robotOperations.excavateStonesForDays(numberOfDays).numberOfStones(),
+                robotOperations.excavateStonesForDays(numberOfDays).robotActions());
+        return new ResponseEntity<>(robotPlan, HttpStatus.OK);
     }
 
     /**
@@ -28,8 +31,9 @@ public final class RobotRestController {
      */
     @PostMapping("/approximation")
     public ResponseEntity<RobotPlan> approximateDays(@RequestParam Integer numberOfStones) {
-        // TODO
-        throw new ErrorResponseException(HttpStatus.I_AM_A_TEAPOT);
+        RobotPlan robotPlan = new RobotPlan(robotOperations.daysRequiredToCollectStones(numberOfStones).numberOfDays(),
+                numberOfStones, robotOperations.daysRequiredToCollectStones(numberOfStones).robotActions());
+        return new ResponseEntity<>(robotPlan, HttpStatus.OK);
     }
 
 }
